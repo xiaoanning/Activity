050202e99e0b8c99d83dc4092cbe2f43730cda0c
xiaoanning <734871221@qq.com>
2017-09-05 14:48:17 +0800

    Initial Commit

---
+++ Activity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Activity
+//
+//  Created by 安宁 on 2017/9/5.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Activity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Activity
+//
+//  Created by 安宁 on 2017/9/5.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
